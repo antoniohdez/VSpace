fb9603543cd699877b8da0a779529b23b2711591
Eros <eros.espinola.gonzalez@gmail.com>
2013-11-09 03:34:49 -0600

    json for gifts

---
+++ jsonGifts.php
@@ -0,0 +1,20 @@
+<?php
+	$con=mysqli_connect("localhost","root","","VSpace");
+	if (mysqli_connect_errno())
+  	{
+  		echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  	}
+	if($result = mysqli_query($con,"SELECT * FROM gifts;"))
+	{
+		while($row = $result->fetch_array(MYSQLI_ASSOC)){
+			$query = mysqli_query($con,"SELECT name FROM users where user_id=".$row["user_id"].";");
+			$user = mysqli_fetch_array($query);
+			$array[] = array("id" => $row["id"], "user" => $user["name"], "gift" => $row["gift"], "message" => $row["message"]);
+		}
+		printf(json_encode(array("dataArray" => $array)));
+	}
+	else{
+		printf("Error");
+	}
+	mysqli_close($con);
+?>
