fd5700bf80481be8202eb80036da90d741b23a61
Eros <eros.espinola.gonzalez@gmail.com>
2013-11-10 12:02:02 -0600

    cambio en el json

--- json.php
+++ json.php
@@ -1,6 +1,5 @@
 <?php
 	$con=mysqli_connect("localhost","root","","VSpace");
-	//$con=mysqli_connect("localhost","fondeand","Parasito#666","fondeand_vspace");
 	if (mysqli_connect_errno())
   	{
   		echo "Failed to connect to MySQL: " . mysqli_connect_error();
@@ -8,7 +7,9 @@
 	if($result = mysqli_query($con,"SELECT * FROM points;"))
 	{
 		while($row = $result->fetch_array(MYSQLI_ASSOC)){
-			$array[] = array("id" => $row["id"], "latitude" => $row["latitude"], "longitude" => $row["longitude"], "feeling" => $row["feeling"], "message" => $row["message"]);
+			$query = mysqli_query($con,"SELECT name FROM users where user_id=".$row["user_id"].";");
+			$user = mysqli_fetch_array($query);
+			$array[] = array("id" => $row["id"], "user" => $user["name"],"latitude" => $row["latitude"], "longitude" => $row["longitude"], "feeling" => $row["feeling"], "message" => $row["message"]);
 		}
 		printf(json_encode(array("dataArray" => $array)));
 	}
